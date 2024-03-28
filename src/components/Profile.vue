82412c43210301e80c1b1891a28819ca9ae82cee
ogaycray <134960256+ogaycray@users.noreply.github.com>
2024-03-28 19:51:07 +0800

    update home button

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,80 +1,88 @@
 <template>
-    <div class="profile-container">
-        <h1> Your Profile </h1>
+    <div class="profile-container" v-if="user">
+        <div>
+            <button id="homebutton" type="button" @click="$router.push('/home')">Home</button>
 
-        <div id="nameCell">
-            <h3>Name</h3>
-            <p id="name"></p>
-        </div>
-        <hr>
+            <h1> Your Profile </h1>
 
-        <div id="mobileCell">
-            <h3>Mobile</h3>
-            <p id="mobile"></p>
-        </div>
-        <hr>
 
-        <div id="emailCell">
-            <h3>Email</h3>
-            <p id="email"></p>
-        </div>
-        <hr>
+            <div id="nameCell">
+                <h3>Name:</h3>
+                <p id="name"> {{ user.displayName }}</p>
+            </div>
+            <hr>
 
-        <div id="birthdateCell">
-            <h3>Birth date</h3>
-            <p id="birthdate"></p>
+
+            <div id="emailCell">
+                <h3>Email:</h3>
+                <p id="email"> {{ user.email }}</p>
+            </div>
+            <hr>
+
+            <div id="useridCell">
+                <h3>User ID:</h3>
+                <p id="email"> {{ user.uid }}</p>
+            </div>
+            <hr>
+
+            <button id="editbutton" type="button" @click="editProfile()">Edit</button>
         </div>
-        <hr>
-        <button id="editbutton" type="button" @click="editProfile()">Edit</button>
+        <Logout/>
     </div>
 </template>
 
+
 <script>
-import firebaseApp from '../firebase.js';
-import { getFirestore } from "firebase/firestore"
-import { doc, setDoc } from "firebase/firestore";
-const db = getFirestore(firebaseApp);
+
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import Logout from '@/components/Logout.vue'
+
 
 export default {
     name: "Profile",
-    mounted() {
-        async function display() {
-            const user = auth.currentUser;
-            if (user) {
-                // Get the user's email address
-                const email = user.email;
-
-                // Retrieve the user's data from the Firestore database
-                let data = await db.collection("Users").doc(email).get();
-
-                // Extract the user's name, mobile number, and birthdate from the retrieved data
-                let name = data.data().Name;
-                let mobile = data.data().Mobile;
-                let birthdate = data.data().birthdate;
-
-                // Update the HTML elements with the user's information
-                let nameCell = document.getElementById("nameCell");
-                let mobileCell = document.getElementById("mobileCell");
-                let birthdateCell = document.getElementById("birthdateCell");
-                nameCell.innerHTML = name;
-                mobileCell.innerHTML = mobile;
-                birthdateCell.innerHTML = birthdate;
-            } else {
-                console.log("No user is signed in.");
-            }
-        }
+    data() { 
+        return { 
+            user: null, 
+        }; 
     },
+    mounted() {
+        const auth = getAuth();
+        onAuthStateChanged (auth, user => {  
+            if (user) {
+                this.user = user;
+            }
+        })
+    },
+
 
     methods: {
         editProfile() {
-            router.push("/edit")
+            this.$router.push("/edit")
         }
     }
 }
 </script>
 
+
 <style>
 .profile-container {
+    font-family: pjs;
     text-align: center;
+}
+
+#nameCell, #useridCell, #emailCell {
+    margin-bottom: 5px;
+}
+
+#homebutton {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background-color: #7F56D9;
+    color: white;
+    padding: 5px 10px;
+    text-align: center;
+    margin: 4px 2px;
+    cursor: pointer;
 }
 </style>

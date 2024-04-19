66017ea2c0f277b1259f21567180e87c246736ed
Edwin <edwinleck@gmail.com>
2024-04-19 11:55:27 +0800

    Added Transaction Table and Add Transaction Button

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,39 @@
+<template>
+  <button id="ATbutton" type="button" @click="emitOpenModal">
+    Add Transaction
+  </button>
+</template>
+
+<script>
+export default {
+  name: "AddTransaction",
+  methods: {
+    emitOpenModal() {
+      // Emit an event that the parent can listen to open the modal
+      this.$emit("openModal");
+    },
+  },
+};
+</script>
+
+<style>
+#ATbutton {
+  font-family: pjs;
+  position: absolute;
+  border: transparent;
+  top: 15px;
+  right: 100px;
+  background-color: #7f56d9;
+  color: white;
+  padding: 5px 10px;
+  text-align: center;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 10px;
+}
+
+button:hover {
+  background-color: transparent;
+  opacity: 0.7;
+}
+</style>

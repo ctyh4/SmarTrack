d79e3e0b98009768ae8d47c12614b347189691fe
ogaycray <134960256+ogaycray@users.noreply.github.com>
2024-04-25 16:31:06 +0800

    Fixed Payment page styling

--- src/components/PaymentForm.vue
+++ src/components/PaymentForm.vue
@@ -60,7 +60,7 @@
       </select>
     </div> 
     <div class = "submit">
-      <button type="submit">Submit Transaction</button>
+      <button id="submitbtn" type="submit">Submit Transaction</button>
     </div>
   </form>
   
@@ -469,36 +469,25 @@ form {
 }
 
 input:hover {
-  box-shadow: 3px 3px plum;
   border-radius: 2px;
 }
 
-.submit{
+#submitbtn{
+  border: transparent;
+  padding: 5px;
   text-align: center;
-  appearance: button;
-  background-color: #4D4AE8;
-  background-image: linear-gradient(180deg, rgba(255, 255, 255, .15), rgba(255, 255, 255, 0));
-  border: 1px solid #4D4AE8;
-  border-radius: 1rem;
-  box-shadow: rgba(255, 255, 255, 0.15) 0 1px 0 inset, rgba(46, 54, 80, 0.075) 0 1px 1px;
-  box-sizing: border-box;
-  color: #FFFFFF;
+  font-family: pjs;
+  align-items: center;
+  justify-content: center;
+  background-color: #7f56d9;
+  color: white;
   cursor: pointer;
-  display: inline-block;
-  font-family: Inter,sans-serif;
-  font-size: 1rem;
-  font-weight: 500;
-  line-height: 1.5;
-  margin: 0;
-  padding: .01rem 0.1rem;
-  text-align: center;
-  text-transform: none;
-  transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-  user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-  vertical-align: middle;
-  
+  border-radius: 5px;
+}
+
+button:hover {
+  background-color: transparent;
+  opacity: 0.7;
 }
       
 option {
